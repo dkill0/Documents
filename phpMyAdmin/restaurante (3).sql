a52eecd6086823969acf37512c97b081a93a7ffd
dkill0 <cpalonso98@hotmail.com>
2021-11-15 11:25:18 +0100

    ff

--- phpMyAdmin/restaurante (3).sql
+++ phpMyAdmin/restaurante (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 12-11-2021 a las 12:26:02
+-- Tiempo de generación: 15-11-2021 a las 11:24:56
 -- Versión del servidor: 5.7.31
 -- Versión de PHP: 7.3.21
 
@@ -67,24 +67,52 @@ CREATE TABLE IF NOT EXISTS `productos` (
   `nombre` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `precio` double NOT NULL,
-  PRIMARY KEY (`idProducto`)
+  `tipo` int(1) DEFAULT NULL,
+  PRIMARY KEY (`idProducto`),
+  KEY `tipo` (`tipo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`idProducto`, `nombre`, `descripcion`, `precio`) VALUES
-(1, 'Lentejas', 'Lentejas ricas', 10),
-(2, 'Pipas', 'pipas', 0),
-(3, 'Jamon', 'plato jamon con keso', 15),
-(4, 'Patatas olivas', 'patatas con olivas', 2),
-(5, 'torta de chicharron', 'una torta de chicharron', 3),
-(6, 'Caminito', 'Cerveza IPA', 4),
-(7, 'Zazu', 'Cerveza Artesana', 4),
-(8, 'Costa Azul', 'Cerveza Artesana', 4),
-(9, 'Almendras', 'Almendras', 3),
-(10, 'Tabla quesos', 'quesos', 8);
+INSERT INTO `productos` (`idProducto`, `nombre`, `descripcion`, `precio`, `tipo`) VALUES
+(1, 'Lentejas', 'Lentejas ricas', 10, 6),
+(2, 'Pipas', 'pipas', 0, 5),
+(3, 'Jamon', 'plato jamon con keso', 15, 5),
+(4, 'Patatas olivas', 'patatas con olivas', 2, 5),
+(5, 'torta de chicharron', 'una torta de chicharron', 3, 7),
+(6, 'Caminito', 'Cerveza IPA', 4, 4),
+(7, 'Zazu', 'Cerveza Artesana', 4, 4),
+(8, 'Costa Azul', 'Cerveza Artesana', 4, 4),
+(9, 'Almendras', 'Almendras', 3, 5),
+(10, 'Tabla quesos', 'quesos', 8, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo`
+--
+
+DROP TABLE IF EXISTS `tipo`;
+CREATE TABLE IF NOT EXISTS `tipo` (
+  `idTipo` int(1) NOT NULL,
+  `descripcion` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idTipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tipo`
+--
+
+INSERT INTO `tipo` (`idTipo`, `descripcion`) VALUES
+(1, 'admin'),
+(2, 'camarero'),
+(3, 'cliente'),
+(4, 'bebida'),
+(5, 'entrante'),
+(6, 'plato'),
+(7, 'postre');
 
 -- --------------------------------------------------------
 
@@ -100,8 +128,9 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `contrasena` varchar(8) CHARACTER SET ucs2 COLLATE ucs2_spanish_ci NOT NULL,
   `tipo` int(1) DEFAULT NULL COMMENT '1 equivale a admin\r\n2 equivale a camarero\r\n3 equivale a cliente',
   PRIMARY KEY (`idUser`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  UNIQUE KEY `email` (`email`),
+  KEY `tipo` (`tipo`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -117,7 +146,8 @@ INSERT INTO `usuario` (`idUser`, `email`, `nombre`, `contrasena`, `tipo`) VALUES
 (7, 'cliente4', 'Juan', '123', 3),
 (9, 'cliente5', 'Pepe', '123', 3),
 (10, 'cliente6', 'Ana', '123', 3),
-(11, 'cliente7', 'Rocio', '123', 3);
+(11, 'cliente7', 'Rocio', '123', 3),
+(12, 'cliente9', 'Alex', '123', 3);
 
 --
 -- Restricciones para tablas volcadas
@@ -136,6 +166,18 @@ ALTER TABLE `lineapedido`
 ALTER TABLE `pedido`
   ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`cliente`) REFERENCES `usuario` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`camarero`) REFERENCES `usuario` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipo` (`idTipo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipo` (`idTipo`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
