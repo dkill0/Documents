2ca728afef966bdc9fa0551bba93c0b5dfe49ecf
dkill0 <cpalonso98@hotmail.com>
2022-02-08 11:37:37 +0100

    Update funciones.js

--- Cliente/java scripts/3raya/script/funciones.js
+++ Cliente/java scripts/3raya/script/funciones.js
@@ -1,14 +1,16 @@
-const tablero = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let tablero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 const imagenesTotales = document.getElementsByTagName("img");
 let conta = 0;
 
 function cambia(nombre) {
-    posicion = nombre.substr(3);
+    posicion = nombre.substr(3) - 1;
     let imagen = document.getElementById(nombre);
     imagen.src = "./img/jedi.png";
     conta++;
     imagen.style.pointerEvents = "none";
-    tablero.splice(posicion, 0);
+    console.log(posicion);
+    tablero.splice(posicion, 1);
+    console.log(tablero);
 
     //metodo que edita el css, inhabilitando los eventos 
     setTimeout(sid, 1000);
@@ -29,15 +31,16 @@ function reinicia(nombre2) {
 
 function sid() {
     let aleatorio;
-    aleatorio = Math.floor(Math.random() * (tablero.length) +
-        1);
+    aleatorio = Math.floor(Math.random() * (tablero.length));
 
     let nombre2 = "img" + aleatorio;
     let imagen2 = document.getElementById(nombre2);
 
-
     imagen2.src = "./img/sith.png";
     imagen2.style.pointerEvents = "none";
 
-    tablero.splice(aleatorio, 0);
+    console.log(aleatorio);
+    tablero.splice(aleatorio, 1);
+    console.log(tablero);
+
 }
