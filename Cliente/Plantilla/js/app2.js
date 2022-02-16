580e789bd2a47e0429114be42b568929517b665c
dkill0 <cpalonso98@hotmail.com>
2022-02-16 14:16:19 +0100

    ghv

--- Cliente/Plantilla/js/app2.js
+++ Cliente/Plantilla/js/app2.js
@@ -2,48 +2,34 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-
 function init() {
     let opcion = document.getElementsByClassName(".tipo").value;
-    fetchData("option");
+    fetchData(opcion);
 
 }
 
-let validos;
 async function fetchData(opcion) {
     try {
         let count = 0;
         let aleatorio;
-
-
         let res;
         let data;
         do {
             aleatorio = getRandomInt(1, 300);
-
-
             res = await fetch("http://acnhapi.com/v1/villagers/" + aleatorio);
             data = await res.json();
             if (data.personality === opcion) {
-                console.log(data.personality);
 
-                count++;
                 pintaCard(data);
+                count++;
             }
-            console.log(count);
-
         } while (count < 10);
-
-
-
-
     } catch (error) {
         console.log(error);
     }
 }
 
 function pintaCard(vecino) {
-    console.log(vecino);
 
     const flex = document.querySelector('.flex');
     const template = document.querySelector('#template-card').content;
@@ -52,8 +38,6 @@ function pintaCard(vecino) {
     clone.querySelector('.card-body-img').setAttribute('src', vecino.image_uri);
     clone.querySelector('.card-body-title').innerHTML = `${vecino.name["name-USen"]}`;
     clone.querySelector('.card-body-text').innerHTML = `"${vecino.saying}"`;
-
-
     clone.querySelectorAll(".card-footer-social h4")[0].textContent = vecino.personality;
     clone.querySelectorAll(".card-footer-social h4")[1].textContent = vecino.birthday;
     clone.querySelectorAll(".card-footer-social h4")[2].textContent = vecino.gender;
